e306cd318081b7641e488bbeff0245febb562d1d
Lucas Gabriel <gabif.johann@gmail.com>
2023-11-28 00:08:40 -0300

    add: schedule form calendar step

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/src/componentes/Calendar'
+import { Container } from './styles'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
