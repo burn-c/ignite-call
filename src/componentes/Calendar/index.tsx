c9c3ad8ea7d45e19cd0b20175dc89bbc003394b2
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-07 22:33:54 -0300

    fix: not show data on loading

--- src/componentes/Calendar/index.tsx
+++ src/componentes/Calendar/index.tsx
@@ -78,6 +78,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   })
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -118,7 +122,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
             .endOf('day')
             .isBefore(
               new Date() ||
-                blockedDates?.blockedWeekDays.includes(date.get('day')),
+                blockedDates.blockedWeekDays.includes(date.get('day')),
             ),
         }
       }),
