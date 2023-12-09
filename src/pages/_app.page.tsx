92db0ae1713c9c7f7c49b951819afda4b56f1abf
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-09 01:48:43 -0300

    add: setup next seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import '../lib/dayjs'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
 import { globalStyles } from '../styles/global'
 import { queryClient } from '../lib/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
@@ -14,6 +15,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
